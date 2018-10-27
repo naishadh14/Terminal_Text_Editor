8983315e860b72308e6a06606aaa17cf4aeac7f3
Naishadh Vora <voranaishadh@gmail.com>
2018-10-27 22:43:08 +0000

    Does basic resizing and prints as a function and can handle lines with size greater than xmax without '\n'.

--- file.c
+++ file.c
@@ -1,11 +1,14 @@
 //Check behavior of lseek(), that's what's causing the problem
 //Also check if line is greater than xmax (without terminating with \n
+//Keep line count if contains only '\n' that might solve the scrolling problem
+//that skips lines with only '\n'
 #include <ncurses.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <signal.h>
 
 int countLine(int fd) {
 	int i, xmax, ymax, flag = 0, j = 0;
@@ -31,7 +34,7 @@ int readLine(int fd, char **lines, int i) {
 	int ymax, xmax, flag = 0, j = 0;
 	char c;
 	getmaxyx(stdscr, ymax, xmax);
-	while(read(fd, &c, 1) && j < xmax) {
+	while(j < xmax - 1 && read(fd, &c, 1)) {
 		if(c == '\n') {
 			lines[i][j] = c;
 			j++;
@@ -42,19 +45,44 @@ int readLine(int fd, char **lines, int i) {
 		j++;	
 	}
 	lines[i][j++] = '\0';
-	if(flag == 0)
+	if(flag == 0 && j != xmax)
 		return INT_MAX;
 	return j;
 }
 
+int printScreen(int fd, int count, char **lines) {
+	int xmax, ymax, check, eof, sum, i;
+	sum = eof = 0;
+	getmaxyx(stdscr, ymax, xmax);
+	lseek(fd, 0, SEEK_SET);
+	for(i = 0; i < count; i++) {
+		sum+= countLine(fd);
+	}
+	move(ymax - 1, 0);
+	lseek(fd, sum, SEEK_SET);
+	move(0, 0);
+	for(i = 0; i < ymax; i++) {
+		check = readLine(fd, lines, i);
+		printw("%s", lines[i]);
+		if(check == INT_MAX) {
+			eof = 1;
+			break;
+		}
+	}
+	return eof;
+}
+
+void resize(int sig) {
+	
+}
+
 int main(int argc, char *argv[]) {
-	int x, y, xmax, ymax, ssize;
+	int x, y, xmax, ymax, ssize, newxmax, newymax;
 	int ch, fd;
-	char **lines, **excess, c;
+	char **lines, c;
 	int flag = 0, count = 0, i = 0, check, eof = 0, sum = 0;
 	/* lines is a array of strings. 
 	* Each string is the characters to be displayed on a line.
-	* excess is just a copy for lines for discard purposes.
 	*/
 	if(argc != 2) {
 		printf("Please enter 2 arguments\n");
@@ -73,27 +101,40 @@ int main(int argc, char *argv[]) {
 	getmaxyx(stdscr, ymax, xmax);
 	ssize = xmax * ymax;
 	lines = (char **)malloc(ymax * sizeof(char *));
-	excess = (char **)malloc(ymax * sizeof(char *));
-	if(lines == NULL || excess == NULL) 
+	if(lines == NULL) 
 		return 1;
 	for(i = 0; i < ymax; i++) {
 		lines[i] = (char *)malloc(xmax + 1);
-		excess[i] = (char *)malloc(xmax + 1);
-		if(lines[i] == NULL || excess[i] == NULL)
+		if(lines[i] == NULL)
 			return 1;
 	}
 	
-	for(i = 0; i < ymax; i++) {
-		check = readLine(fd, lines, i);
-		printw("%s", lines[i]);
-		if(check == INT_MAX) {
-			eof = 1;
-			break;
-		}
-	}
-	
+	printScreen(fd, count, lines);
 	while(1) {
 		ch = getch();
+		//signal(SIGWINCH, resize);
+		/*
+		getmaxyx(stdscr, newymax, newxmax);
+		if(newxmax != xmax || newymax != ymax) {
+			ymax = newymax;
+			xmax = newxmax;
+			ssize = xmax * ymax;
+			for(i = 0; i < ymax; i++) {
+				free(lines[i]);
+				lines[i] = (char *)malloc(xmax + 1);
+				if(lines[i] == NULL)
+					return 1;
+			}
+			for(i = 0; i < ymax; i++) {
+				check = readLine(fd, lines, i);
+				printw("%s", lines[i]);
+				if(check == INT_MAX) {
+					eof = 1;
+					break;
+				}
+			}
+		}
+		*/
 		getyx(stdscr, y, x);
 		switch(ch) {
 			case KEY_LEFT:
@@ -109,22 +150,7 @@ int main(int argc, char *argv[]) {
 					eof = 0;
 					clear();
 					count--;
-					sum = 0;
-					lseek(fd, 0, SEEK_SET);
-					for(i = 0; i < count; i++) {
-						sum+= countLine(fd);
-					}
-					move(ymax - 1, 0);
-					lseek(fd, sum, SEEK_SET);
-					move(0, 0);
-					for(i = 0; i < ymax; i++) {
-						check = readLine(fd, lines, i);
-						printw("%s", lines[i]);
-						if(check == INT_MAX) {
-							eof = 1;
-							break;
-						}
-					}
+					eof = printScreen(fd, count, lines);
 					move(0, x);
 				}
 				else {
@@ -137,25 +163,8 @@ int main(int argc, char *argv[]) {
 						break;
 					}
 					clear();
-					//move(0, 0);
 					count++;
-					sum = 0;
-					lseek(fd, 0, SEEK_SET);
-					for(i = 0; i < count; i++) {
-						sum+= countLine(fd);
-					}
-					move(ymax - 1, 0);
-					lseek(fd, sum + 1, SEEK_SET);
-					move(0, 0);
-					for(i = 0; i < ymax; i++) {
-						check = readLine(fd, lines, i);
-						printw("%s", lines[i]);
-						if(check == INT_MAX) {
-							eof = 1;
-							break;
-						}
-					}
-					
+					eof = printScreen(fd, count, lines);
 				}
 				else {
 					move(y + 1, x);
@@ -169,8 +178,14 @@ int main(int argc, char *argv[]) {
 				printw(" ");
 				move(y, x);
 				break;
-			case 'A':
-				flag = 1;
+			case KEY_RESIZE:
+				getmaxyx(stdscr, ymax, xmax);
+				for(i = 0; i < ymax; i++) {
+					lines[i] = (char *)realloc(lines[i], xmax + 1);
+					if(lines[i] == NULL)
+						return 1;
+				}
+				printScreen(fd, count, lines);
 				break;
 			default:
 				printw("%c", ch);
